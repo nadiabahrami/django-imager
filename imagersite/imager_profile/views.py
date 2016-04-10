4bec867662787ab66d965bae8106ff29a34cf7c4
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-09 20:30:14 -0700

    Started the URL Routing process follwoing the tutorial.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to imager profile page.  Make a profile or you know use and already established photo upload site.")
