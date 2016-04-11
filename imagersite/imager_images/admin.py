01ba3066f3d24fe1370d24e6c4d4866c309d3e42
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-11 16:46:33 -0700

    models are there.

--- imagersite/imager_images/admin.py
+++ imagersite/imager_images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Photo
+from .models import Album
+
+admin.site.register(Photo)
+admin.site.register(Album)
--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'imager_profile',
+    'imager_images',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#media
+MEDIA_DRIECTORY = 'media'
+MEDIA_URL = '/media/'
