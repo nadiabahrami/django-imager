875b5a248de3e68183f676519736017ab3b3c155
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-11 11:33:03 -0700

    Model not erroring.  Definetly not right though.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    profile = models.ForeignKey(User, unique=True)
+    camera_type = models.CharField(('nikkon'), max_length=30, blank=True)
+    address = models.CharField(('address'), max_length=60, blank=True)
+    web_link = models.CharField(('personal site'), max_length=70, blank=True)
+    photo_type = models.CharField(('nature'), max_length=30, blank=True)
+    social_media = models.CharField(('handle'), max_length=30, blank=True)
+
+    @property
+    def is_active(self):
+        return self.user.is_active
+    
