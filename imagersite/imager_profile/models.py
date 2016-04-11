d41cc1539474ab26689fac4fb5370c022f919b68
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-11 12:01:22 -0700

    Replaced foreign key with a one to one field.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class UserProfile(models.Model):
-    profile = models.ForeignKey(User, unique=True)
+    profile = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     camera_type = models.CharField(('nikkon'), max_length=30, blank=True)
     address = models.CharField(('address'), max_length=60, blank=True)
     web_link = models.CharField(('personal site'), max_length=70, blank=True)
