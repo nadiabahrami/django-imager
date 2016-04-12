d6b5b2000d614e365ef77d6f53a53cfa5e148b4f
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-11 18:57:00 -0700

    Wrote a test.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,11 +1,32 @@
 from django.test import TestCase
+import factory
+from imager_profile.models import User
 
 # Create your tests here.
+
+# class ProfileTestCase2(TestCase):
+
+#     def setUp(self):
+#         self.username = 'boo'
+
+#     def test_foo(self):
+#         self.assertEquals(self.username, 'boo')
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = 'bob'
+    email = 'bob@example.com'
+
 
 class ProfileTestCase(TestCase):
 
     def setUp(self):
-        self.username = 'boo'
+        self.user = UserFactory.create()
+        self.user.set_password('secret')
+        self.user.save()
 
-    def test_foo(self):
-        self.assertEquals(self.username, 'boo')
+    def test_profile(self):
+        self.assertTrue(self.user.profile)
