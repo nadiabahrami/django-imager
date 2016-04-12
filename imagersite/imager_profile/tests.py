2bad0c0fffdb174007592ae4a873736bae707c1a
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-11 19:04:03 -0700

    Wrote another test.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -4,13 +4,14 @@ from imager_profile.models import User
 
 # Create your tests here.
 
-# class ProfileTestCase2(TestCase):
+class ProfileTestCase2(TestCase):
 
-#     def setUp(self):
-#         self.username = 'boo'
+    def setUp(self):
+        self.username = 'boo'
 
-#     def test_foo(self):
-#         self.assertEquals(self.username, 'boo')
+    def test_foo(self):
+        self.assertEquals(self.username, 'boo')
+
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -19,6 +20,7 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     username = 'bob'
     email = 'bob@example.com'
+
 
 
 class ProfileTestCase(TestCase):
@@ -30,3 +32,6 @@ class ProfileTestCase(TestCase):
 
     def test_profile(self):
         self.assertTrue(self.user.profile)
+
+    def test_active(self):
+        self.assertTrue(self.user.is_active)
