792092bad1823252fd74bf77fc2c5d9ef6f805f0
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-19 15:28:12 -0700

    Routes are working. Album pictures not hooked up.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,12 +1,13 @@
 # -*- Coding:Utf-8 -*-
 """Module for my url routing."""
 from django.conf.urls import url
-from .views import LibraryView
+from .views import LibraryView, AlbumView
 from django.contrib.auth.decorators import login_required
 
 # from . import views
 
 
 urlpatterns = [
-    url(r'^$', login_required(LibraryView.as_view()), name='library_view'),
+    url(r'^library/', login_required(LibraryView.as_view()), name='library_view'),
+    url(r'^album/(?P<user_id>[0-9])/(?P<pk>[0-9])', login_required(AlbumView.as_view()), name='album_view'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.views.generic.detail import DetailView
 from imager_images.models import Photo, Album
 from django.views.generic import TemplateView
 
@@ -12,3 +13,15 @@ class LibraryView(TemplateView):
         imgs = Photo.public.filter(owner=user.pk)
         albums = Album.all_albums.filter(owner=user.pk)
         return {'imgs': imgs, 'albums': albums}
+
+
+class AlbumView(DetailView):
+    template_name = 'album.html'
+
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        """Return a random public photo for homepage."""
+        user = self.request.user
+        context = super(AlbumView, self).get_context_data(**kwargs)
+        return context
