8465cafc54f9850cac10735494266df931363591
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-19 14:19:04 -0700

    lofin required to see profile now.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -2,9 +2,10 @@
 """Module for my url routing."""
 from django.conf.urls import url
 from .views import ProfileView
+from django.contrib.auth.decorators import login_required
 
 # from . import views
 
 urlpatterns = [
-    url(r'^$', ProfileView.as_view(), name='profile_view'),
+    url(r'^$', login_required(ProfileView.as_view()), name='profile_view'),
 ]
