916edc4adacfc4ced81a4b496a0c682509d31127
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-19 14:17:42 -0700

    Login required diverting is working from Library.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -2,10 +2,11 @@
 """Module for my url routing."""
 from django.conf.urls import url
 from .views import LibraryView
+from django.contrib.auth.decorators import login_required
 
 # from . import views
 
 
 urlpatterns = [
-    url(r'^$', LibraryView.as_view(), name='library_view'),
+    url(r'^$', login_required(LibraryView.as_view()), name='library_view'),
 ]
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.contrib import admin
 from .views import ClassView
 from django.conf.urls.static import static
-from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
