fcd0653a104d000b82e14e75d67b7cc89e292738
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-19 20:43:20 -0700

    All the photos are user only.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     url(r'^photos/(?P<user_id>[0-9]+)/(?P<pk>[0-9]+)', login_required(PhotoView.as_view()), name='photo_view'),
     url(r'^albums/add/', login_required(add_album), name='create_al_view'),
     url(r'^photos/add/', login_required(add_photo), name='add_pho_view'),
+    url(r'^albums/(?P<pk>[0-9]+)/edit/', login_required(add_album), name='create_al_view'),
+    url(r'^photos/(?P<pk>[0-9]+)/edit/', login_required(add_photo), name='add_pho_view'),
 ]
