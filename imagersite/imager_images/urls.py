3d1954554db705d3712691ffd84b3f6c83461f47
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-19 17:16:16 -0700

    Album views working and hooked up.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -10,4 +10,5 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     url(r'^library/', login_required(LibraryView.as_view()), name='library_view'),
     url(r'^album/(?P<user_id>[0-9])/(?P<pk>[0-9])', login_required(AlbumView.as_view()), name='album_view'),
+    url(r'^picture/(?P<user_id>[0-9])/(?P<pk>[0-9])', login_required(PictureView.as_view()), name='picture_view'),
 ]
