9c53fcef22a82426e5dfd7ff96366620786a6177
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-20 19:56:42 -0700

    Profile editing is working.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 from imager_images.models import Photo, Album
-from imager_profile.models import UserProfile, EditProfile
+from imager_profile.models import UserProfile, EditProfile, EditUser
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 class ProfileView(TemplateView):
@@ -19,30 +20,20 @@ class ProfileView(TemplateView):
 
 def edit_profile(request):
     if request.method == 'POST':
-        u = UserProfile.objects.get(user=request.user)
-        form_profile = EditProfile(request.POST, instance=u)
-        if form_profile.is_valid():
+        up = UserProfile.objects.get(user=request.user)
+        form_profile = EditProfile(request.POST, instance=up)
+        u = User.objects.get(profile=up)
+        form_user = EditUser(request.POST, instance=u)
+        if form_profile.is_valid() and form_user.is_valid():
             form_profile.instance.user = request.user
+            form_user.instance.user = request.user
             form_profile.save()
+            form_user.save()
             return HttpResponseRedirect('/profile/')
-        return render(request, 'edit_profile.html', context={'form_profile': form_profile})
+        return render(request, 'edit_profile.html', context={'form_profile': form_profile,  'form_user': form_user})
     else:
-        u = UserProfile.objects.get(user=request.user)
-        form_profile = EditProfile(instance=u)
-        return render(request, 'edit_profile.html', context={'form_profile': form_profile})
-
-
-# def update_settings(request):
-#     if request.method== 'POST':
-#         u = UserProfile.objects.get(user=request.user)
-#         form = UserProfile(request.POST, instance=u)
-#         if form.is_valid():
-#             profile = form.save(commit=False)
-#             profile.user = request.user
-#             profile.save()
-#             return HttpResponseRedirect('/profile/')
-#         return render(request, 'edit_profile.html', context={'form_profile': form_profile})
-#     else:
-#         u = UserProfile.objects.get(user=request.user)
-#         form = UserProfileForm(instance=u) #No request.POST
-#         return render(request, 'edit_profile.html', context={'form_profile': form_profile})
+        up = UserProfile.objects.get(user=request.user)
+        form_profile = EditProfile(instance=up)
+        u = User.objects.get(profile=up)
+        form_user = EditUser(instance=u)
+        return render(request, 'edit_profile.html', context={'form_profile': form_profile, 'form_user': form_user})
