596a6e5ae6e9b22126cdfa2f93f0c0630f764d10
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-20 18:58:45 -0700

    URL route and begining view made.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,11 +1,12 @@
 # -*- Coding:Utf-8 -*-
 """Module for my url routing."""
 from django.conf.urls import url
-from .views import ProfileView
+from .views import ProfileView, edit_profile
 from django.contrib.auth.decorators import login_required
 
 # from . import views
 
 urlpatterns = [
     url(r'^$', login_required(ProfileView.as_view()), name='profile_view'),
+    url(r'^edit/', login_required(edit_profile), name='create_al_view'),
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -12,3 +12,18 @@ class ProfileView(TemplateView):
         photo_count = Photo.public.filter(owner=user.pk).count()
         album_count = Album.all_albums.filter(owner=user.pk).count()
         return {'photo_count': photo_count, 'album_count': album_count}
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user_album = Album.all_albums.get(id=pk)
+        form = CreateAlbum(request.POST, instance=user_album)
+        if form.is_valid():
+            form.instance.owner = request.user
+            form.save()
+            return HttpResponseRedirect('/profile/')
+        return render(request, 'edit_album.html', context={'form': form})
+    else:
+        user_album = Album.all_albums.get(id=pk)
+        form = CreateAlbum(instance=user_album)
+        return render(request, 'edit_album.html', context={'form': form})
