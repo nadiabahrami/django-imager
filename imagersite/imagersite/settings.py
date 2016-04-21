8393927fdb73af24db879cf2aba2f2b2be3fcc77
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-21 13:52:19 -0700

    Prepping for AWS deployment

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -99,19 +99,19 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'imager',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imagerdb',
+        'USER': 'imageruesr',
+        'PASSWORD': 'supersecret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
