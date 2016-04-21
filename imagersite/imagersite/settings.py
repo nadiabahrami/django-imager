8c4f5afe4d290b6c6b170756a5d3e6e099fd297c
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-21 13:43:05 -0700

    Prepping for AWS deployment

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@wf0x!oss73*bq*-2=#mphhc1-4rvfk)^2hk725yqrqj=773a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+THUMBNAIL_DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -89,28 +91,27 @@ CACHES = {
     }
 }
 
-THUMBNAIL_DEBUG = True
 
-# THUMBNAIL_CONVERT
 
-# THUMBNAIL_IDENTIFY
 
 WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'imager',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'imager',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
