437c71faa224292af40893c94713107890ad77af
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-21 15:56:22 -0700

    Prepping for AWS deployment

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
